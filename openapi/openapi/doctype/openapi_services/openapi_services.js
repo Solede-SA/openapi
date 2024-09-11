921634cd642e519ab108ab56b3194376f7e0daca
Lorenzo Caldara <lorenzo.caldara@gmail.com>
2024-09-11 16:29:16 +0200

    Passi avanti

--- openapi/openapi/doctype/openapi_services/openapi_services.js
+++ openapi/openapi/doctype/openapi_services/openapi_services.js
@@ -1,10 +1,8 @@
 // Copyright (c) 2024, Solede SA and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("OpenApiSettings", {
+// frappe.ui.form.on("OpenApi Services", {
 // 	refresh(frm) {
 
 // 	},
 // });
-
-
--- openapi/openapi/doctype/openapi_services/test_openapi_services.py
+++ openapi/openapi/doctype/openapi_services/test_openapi_services.py
@@ -5,5 +5,5 @@
 from frappe.tests.utils import FrappeTestCase
 
 
-class TestOpenApiSettings(FrappeTestCase):
+class TestOpenApiServices(FrappeTestCase):
 	pass
