a5c89681f60368f34b09f51ccd4116774a46b798
Lorenzo Caldara <lorenzo.caldara@gmail.com>
2024-09-17 23:35:00 +0200

    Altri passaggi per lo sdi

--- openapi/openapi/doctype/notifiche_sistema_di_interscambio/notifiche_sistema_di_interscambio.py
+++ openapi/openapi/doctype/notifiche_sistema_di_interscambio/notifiche_sistema_di_interscambio.py
@@ -5,5 +5,5 @@
 from frappe.model.document import Document
 
 
-class NotificheSDI(Document):
+class Notifichesistemadiinterscambio(Document):
 	pass
---
+++ openapi/openapi/doctype/transazioni_sdi/transazioni_sdi.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Solede SA and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Transazioni SDI", {
+// 	refresh(frm) {
+
+// 	},
+// });
